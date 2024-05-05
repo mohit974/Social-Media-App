d4a18bc68ccc16b2e68122e9aa1c81e24c24c60e
mohit074 <mohitgandhi0011@gmail.com>
2024-05-05 17:47:05 +0530

    deployed server endpoint - added

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// const API = axios.create({
-//   baseURL: "https://social-media-app-qdki.onrender.com/",
-// });
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({
+  baseURL: "https://social-media-app-qdki.onrender.com/",
+});
+// const API = axios.create({ baseURL: "http://localhost:5000" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
